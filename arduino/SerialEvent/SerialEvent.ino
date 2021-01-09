dc6e39ebb343d1cf681d576e097ebd23bd279dbd
csalinasonline <csalinasonline@gmail.com>
2021-01-09 03:51:49 -0800

    Updated Arduino header.

--- arduino/SerialEvent/SerialEvent.ino
+++ arduino/SerialEvent/SerialEvent.ino
@@ -3,18 +3,15 @@
 
   When new serial data arrives, this sketch adds it to a String.
   When a newline is received, the loop prints the string and clears it.
-
-  A good test for this is to try it with a GPS receiver that sends out
-  NMEA 0183 sentences.
+  This example is extended to control a NES Classic modified controller.
 
   NOTE: The serialEvent() feature is not available on the Leonardo, Micro, or
   other ATmega32U4 based boards.
 
-  created 9 May 2011
-  by Tom Igoe
+  created 09 Jan 2021
+  by Conrad Salinas
 
   This example code is in the public domain.
-
   http://www.arduino.cc/en/Tutorial/SerialEvent
 */
 

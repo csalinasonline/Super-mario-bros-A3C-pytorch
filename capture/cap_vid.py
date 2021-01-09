c7eb881003de0c36154adf11be1b51d1b0b0153f
csalinasonline <csalinasonline@gmail.com>
2021-01-09 03:29:12 -0800

    Added correct capture resolution.

--- capture/cap_vid.py
+++ capture/cap_vid.py
@@ -8,8 +8,8 @@ if not cap.isOpened():
    print("Could not open video device")
 
 #To set the resolution
-#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while(True):
    # Capture frame-by-frame

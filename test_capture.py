4657e992a7ad2f064b388f51fb1ad2c4c6ae6f09
csalinasonline <csalinasonline@gmail.com>
2021-01-19 00:05:29 -0800

    Added histogram matching to nes image from sim image

--- test_capture.py
+++ test_capture.py
@@ -28,6 +28,103 @@ CONST_OFFSET_RES_HEIGHT = (102, 539)
 CONST_FEATURE_RES_WIDTH = 84
 CONST_FEATURE_RES_HEIGHT = 84
 
+
+def calculate_cdf(histogram):
+    """
+    This method calculates the cumulative distribution function
+    :param array histogram: The values of the histogram
+    :return: normalized_cdf: The normalized cumulative distribution function
+    :rtype: array
+    """
+    # Get the cumulative sum of the elements
+    cdf = histogram.cumsum()
+ 
+    # Normalize the cdf
+    normalized_cdf = cdf / float(cdf.max())
+ 
+    return normalized_cdf
+ 
+def calculate_lookup(src_cdf, ref_cdf):
+    """
+    This method creates the lookup table
+    :param array src_cdf: The cdf for the source image
+    :param array ref_cdf: The cdf for the reference image
+    :return: lookup_table: The lookup table
+    :rtype: array
+    """
+    lookup_table = np.zeros(256)
+    lookup_val = 0
+    for src_pixel_val in range(len(src_cdf)):
+        lookup_val
+        for ref_pixel_val in range(len(ref_cdf)):
+            if ref_cdf[ref_pixel_val] >= src_cdf[src_pixel_val]:
+                lookup_val = ref_pixel_val
+                break
+        lookup_table[src_pixel_val] = lookup_val
+    return lookup_table
+ 
+def match_histograms(src_image, ref_image):
+    """
+    This method matches the source image histogram to the
+    reference signal
+    :param image src_image: The original source image
+    :param image  ref_image: The reference image
+    :return: image_after_matching
+    :rtype: image (array)
+    """
+    # # Split the images into the different color channels
+    # # b means blue, g means green and r means red
+    # src_b, src_g, src_r = cv2.split(src_image)
+    # ref_b, ref_g, ref_r = cv2.split(ref_image)
+ 
+    # # Compute the b, g, and r histograms separately
+    # # The flatten() Numpy method returns a copy of the array c
+    # # collapsed into one dimension.
+    # src_hist_blue, bin_0 = np.histogram(src_b.flatten(), 256, [0,256])
+    # src_hist_green, bin_1 = np.histogram(src_g.flatten(), 256, [0,256])
+    # src_hist_red, bin_2 = np.histogram(src_r.flatten(), 256, [0,256])    
+    # ref_hist_blue, bin_3 = np.histogram(ref_b.flatten(), 256, [0,256])    
+    # ref_hist_green, bin_4 = np.histogram(ref_g.flatten(), 256, [0,256])
+    # ref_hist_red, bin_5 = np.histogram(ref_r.flatten(), 256, [0,256])
+
+    src_hist, bin_0 = np.histogram(src_image.flatten(), 256, [0,256]) 
+    ref_hist, bin_1 = np.histogram(ref_image.flatten(), 256, [0,256])      
+ 
+    # Compute the normalized cdf for the source and reference image
+    # src_cdf_blue = calculate_cdf(src_hist_blue)
+    # src_cdf_green = calculate_cdf(src_hist_green)
+    # src_cdf_red = calculate_cdf(src_hist_red)
+    # ref_cdf_blue = calculate_cdf(ref_hist_blue)
+    # ref_cdf_green = calculate_cdf(ref_hist_green)
+    # ref_cdf_red = calculate_cdf(ref_hist_red)
+
+    src_cdf = calculate_cdf(src_hist)
+    ref_cdf = calculate_cdf(ref_hist)
+
+    # # # Make a separate lookup table for each color
+    # # blue_lookup_table = calculate_lookup(src_cdf_blue, ref_cdf_blue)
+    # # green_lookup_table = calculate_lookup(src_cdf_green, ref_cdf_green)
+    # # red_lookup_table = calculate_lookup(src_cdf_red, ref_cdf_red)
+
+    lookup_table = calculate_lookup(src_cdf, ref_cdf)
+ 
+    # # Use the lookup function to transform the colors of the original
+    # # source image
+    # blue_after_transform = cv2.LUT(src_b, blue_lookup_table)
+    # green_after_transform = cv2.LUT(src_g, green_lookup_table)
+    # red_after_transform = cv2.LUT(src_r, red_lookup_table)
+    after_transform = cv2.LUT(src_image, lookup_table)
+ 
+    # Put the image back together
+    # image_after_matching = cv2.merge([
+    #     blue_after_transform, green_after_transform, red_after_transform])
+    # image_after_matching = cv2.convertScaleAbs(image_after_matching)
+
+    image_after_matching = after_transform
+    image_after_matching = cv2.convertScaleAbs(image_after_matching)    
+ 
+    return lookup_table, image_after_matching
+
 #
 def get_args():
     parser = argparse.ArgumentParser(
@@ -194,6 +291,7 @@ def test(opt):
     #print(state.shape)
     #print(type(state))
     img_1 = convert_state_to_img(state2)
+    img_1b = img_1
     print(img_1)
     print(img_1.shape)
     time.sleep(2)
@@ -231,12 +329,17 @@ def test(opt):
         #
         frame_5 = cv2.resize(frame_4, (CONST_FEATURE_RES_WIDTH, CONST_FEATURE_RES_HEIGHT))
         #
-        frame_5 = cv2.add(frame_5, b)
+        #frame_5 = cv2.add(frame_5, b)
         st = frame_5
         img_2 = frame_5
+        img_2b = frame_5
         cv2.imwrite('nes_img_1.png', frame_5)
         a[0,i,...] = st
         print(f'{i}:{a.shape}')
+
+
+    hist_lut, output_image = match_histograms(img_1b, img_2b)
+    cv2.imwrite('nes_cor_img_1.png', output_image)
 
     print(img_2)
     print(img_2.shape)
@@ -313,8 +416,8 @@ def test(opt):
             #
             frame_3 = frame_2[CONST_OFFSET_RES_WIDTH[0]:CONST_OFFSET_RES_WIDTH[1], CONST_OFFSET_RES_HEIGHT[0]:CONST_OFFSET_RES_HEIGHT[1]]
             #
-            #c[0:15] = (157)
-            #c[15:473:,:] = frame_3
+            c[0:15] = (157)
+            c[15:473:,:] = frame_3
             #print(frame_3.shape)     
             #       
             frame_4 = cv2.resize(frame_3, (CONST_NES_RES_WIDTH, CONST_NES_RES_HEIGHT))
@@ -327,9 +430,12 @@ def test(opt):
             #
             frame_5 = cv2.resize(frame_4, (CONST_FEATURE_RES_WIDTH, CONST_FEATURE_RES_HEIGHT))
             #
-            frame_5 = cv2.add(frame_5, b)
+            #frame_5 = cv2.add(frame_5, b)
             #
-            st = frame_5
+            after_transform = cv2.LUT(frame_5, hist_lut)
+            frame_6 = cv2.convertScaleAbs(after_transform)             
+            #
+            st = frame_6
             a[0,i,...] = st
             #print(f'{i}:{a.shape}')
 
